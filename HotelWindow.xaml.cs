a7bbf4f8e6ba2fdffc146572143eabd250f5b999
gelickk <81441148+gelickk@users.noreply.github.com>
2021-11-07 14:36:25 +0300

    Add files via upload

---
+++ HotelWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PuteshestviePoRossiiGilyazov
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelWindow.xaml
+    /// </summary>
+    public partial class HotelWindow : Window
+    {
+        public static TestEntities _context = new TestEntities();
+        private Hotel _hotel;
+        private int _currentPage = 1;
+        private int _maxPage = 0;
+        public HotelWindow()
+        {
+            try
+            {
+                InitializeComponent();
+
+                RefreshHotels();
+                this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void RefreshHotels()
+        {
+            try
+            {
+                DataGridHotels.ItemsSource = _context.Hotel.OrderBy(h => h.Name).ToList();
+                _maxPage = Convert.ToInt32(Math.Ceiling(_context.Hotel.ToList().Count * 1.0 / 10));
+
+                var listHotels = _context.Hotel.ToList().Skip((_currentPage - 1) * 10).Take(10).ToList();
+
+                LblTotalPages.Content = "of " + _maxPage.ToString();
+                TxtCurrentPageNumber.Text = _currentPage.ToString();
+                DataGridHotels.ItemsSource = listHotels;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnEditHotelInfo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                EditHotelInfoWindow editHotelInfoWindow = new EditHotelInfoWindow(_context, sender, this);
+                editHotelInfoWindow.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void GoFirstPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _currentPage = 1;
+                RefreshHotels();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void GoPrevPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_currentPage - 1 < 1)
+                {
+                    return;
+                }
+                _currentPage = _currentPage - 1;
+                RefreshHotels();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void TxtCurrentPageNumber_TextChanged(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_currentPage > 0 && _currentPage <= _maxPage && TxtCurrentPageNumber.Text != "")
+                {
+                    if(int.Parse(TxtCurrentPageNumber.Text) <= _maxPage)
+                    {
+                        _currentPage = Convert.ToInt32(TxtCurrentPageNumber.Text);
+                        RefreshHotels();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Такой страницы нет!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void GoNextPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_currentPage + 1 > _maxPage)
+                {
+                    return;
+                }
+                _currentPage = _currentPage + 1;
+                RefreshHotels();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void GoLastPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _currentPage = _maxPage;
+                RefreshHotels();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnAddHotel_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                AddHotelWindow addHotelWindow = new AddHotelWindow(_context, this);
+                addHotelWindow.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PuteshestviePoRossiiGilyazov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TestEntities _context = new TestEntities();
+        private List<Tour> _tours = new List<Tour>();
+        private string _SelectedType;
+        private string _FindedName;
+        public MainWindow()
+        {
+            try
+            {
+                InitializeComponent();
+                ListTours.ItemsSource = _context.Tour.OrderBy(tour => tour.Name).ToList();
+
+                List<Type> types = new List<Type>();
+                types.Add(new Type() { Name = "Все типы" });
+                types.AddRange(_context.Type.OrderBy(t => t.Name).ToList());
+
+                CmbType.ItemsSource = types;
+
+                this._tours = _context.Tour.ToList();
+                this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void TxtFindedTourName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                _FindedName = TxtFindedTourName.Text;
+
+                _tours = _context.Tour.OrderBy(t => t.Name).ToList();
+
+                RefreshTours();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void RefreshTours()
+        {
+            try
+            {
+                if (CmbType.SelectedItem != null)
+                {
+                    if ((CmbType.SelectedItem as Type).Name != "Все типы")
+                    {
+                        Type type = CmbType.SelectedItem as Type;
+                        _SelectedType = type.Name;
+                        _tours = (from t in _tours
+                                  from tn in t.Type
+                                  where tn.Name == _SelectedType
+                                  select t).ToList();
+                    }
+                    else if ((CmbType.SelectedItem as Type).Name == "Все типы")
+                    {
+                        _tours = _context.Tour.OrderBy(t => t.Name).ToList();
+                    }
+                }
+
+                if (TxtFindedTourName.Text != "")
+                {
+                    _tours = _tours.OrderBy(t => t.Name).Where(t => t.Name.ToLower().Contains(_FindedName)).ToList();
+                }
+
+                if ((bool)ChbActual.IsChecked)
+                {
+                    _tours = _tours.OrderBy(t => t.Name).Where(t => t.IsActual == true).ToList();
+                }
+
+                ListTours.ItemsSource = _tours;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void CmbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                _tours = _context.Tour.OrderBy(t => t.Name).ToList();
+
+                RefreshTours();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ChbActual_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _tours = _context.Tour.OrderBy(t => t.Name).ToList();
+                RefreshTours();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ChbActual_Unchecked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _tours = _context.Tour.OrderBy(t => t.Name).ToList();
+                RefreshTours();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnShowHotelWindow_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                HotelWindow window = new HotelWindow();
+                window.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
